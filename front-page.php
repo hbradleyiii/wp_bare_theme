2295878f18e771ba93b9571be1d1228dfa4b58f1
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-11-10 15:21:54 -0500

    Adds bare front-page template.

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php wp_template(__FILE__, true);
+
+////////////////////////////////////////////////////////////
+// front-page.php - default home page
+
+get_header(); ?>
+
+    <main>
+    </main>
+
+<?php get_footer(); ?>
