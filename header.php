5c0363e9148fe212555ea714ab17b6a52724976f
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-11-11 14:42:58 -0500

    Removes title tag from header.
    Because title tag is called in functions.php.

--- header.php
+++ header.php
@@ -8,7 +8,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
-    <title><?php wp_title( ' | ', true, 'right' ); ?>Title</title>
     <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico" />
     <?php wp_head(); ?>
 </head>
