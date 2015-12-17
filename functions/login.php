218b51acc91c2513f067d630874529d01c4ddb46
Harold Bradley III <hbradleyiii@bradleystudio.net>
2015-12-17 16:20:51 -0500

    Corrects css in login script to use sass.

--- functions/login.php
+++ functions/login.php
@@ -5,7 +5,7 @@
 
 
 add_action('login_enqueue_scripts', function() {
-	wp_register_style('style', get_template_directory_uri().'/css/style.css.php');
+	wp_register_style('style', get_template_directory_uri().'/style.css');
 	wp_register_style('roboto-font', 'https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic');
 	wp_enqueue_style('style');
 	wp_enqueue_style('roboto-font');
@@ -17,7 +17,7 @@ add_filter('login_headertitle', function() { return get_bloginfo('description');
 
 add_filter('login_redirect', function( $redirect_to, $request, $user ) {
     if ( isset( $user->roles ) && is_array( $user->roles ) ) {
-        if( in_array('administrator', $user->roles)) { return admin_url(); } 
+        if( in_array('administrator', $user->roles)) { return admin_url(); }
     }
     return $redirect_to;
 }, 10, 3);
