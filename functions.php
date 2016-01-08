71f9b577e7b69465dd9ff56d32902a6033d00b34
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-01-08 12:02:15 -0500

    Adds a hook to change the elipsis to a [read more] link.

--- functions.php
+++ functions.php
@@ -21,22 +21,22 @@ add_action('wp_enqueue_scripts', function() {
     global $wp_styles;
 
     $template_dir_uri = get_template_directory_uri();
-	
-    // Scripts
-	wp_register_script('scripts', $template_dir_uri.'/scripts.js', array('jquery'), false, true);
-	wp_register_script('contact-form', $template_dir_uri.'/contact-form.js', array('jquery'), false, true);
 
-	wp_enqueue_script('scripts');
-	wp_enqueue_script('contact-form');
+    // Scripts
+    wp_register_script('scripts', $template_dir_uri.'/scripts.js', array('jquery'), false, true);
+    wp_register_script('contact-form', $template_dir_uri.'/contact-form.js', array('jquery'), false, true);
+
+    wp_enqueue_script('scripts');
+    wp_enqueue_script('contact-form');
 
     // Styles
-	wp_enqueue_style( 'theme_style', get_stylesheet_uri() );
-	wp_register_style('roboto-font', 'https://fonts.googleapis.com/css?family=Roboto:100,300,300italic,400,700,700italic');
+    wp_enqueue_style( 'theme_style', get_stylesheet_uri() );
+    wp_register_style('roboto-font', 'https://fonts.googleapis.com/css?family=Roboto:100,300,300italic,400,700,700italic');
 
-	wp_enqueue_style('style');
-	wp_enqueue_style('roboto-font');
+    wp_enqueue_style('style');
+    wp_enqueue_style('roboto-font');
 
-	if( is_front_page() ) {
+    if( is_front_page() ) {
         // Front page
     }
 
@@ -44,20 +44,23 @@ add_action('wp_enqueue_scripts', function() {
 
 add_action( 'after_setup_theme', function() {
     add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'title-tag' );
-	add_theme_support( 'post-thumbnails' );
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
-	) );
-	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
-	) );
+    add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'html5', array(
+        'search-form', 'comment-form', 'comment-list', 'gallery', 'caption'
+    ) );
+    add_theme_support( 'post-formats', array(
+        'aside', 'image', 'video', 'quote', 'link', 'gallery', 'status', 'audio', 'chat'
+    ) );
 });
 
 // When ContactForm 7 is initialized, enqueue contact_form script in footer
-add_action( 'wpcf7_enqueue_scripts', function() { 
+add_action( 'wpcf7_enqueue_scripts', function() {
     wp_enqueue_script('contact-form', get_template_directory_uri().'/contact-form.js', false, false, true);
 } );
+
+// Replace excerpt ellipse with 'Read more...' link
+add_filter('excerpt_more', function( $text ) { return str_replace('[&hellip;]', ' [<a href="'.get_permalink().'">Read more...</a>]', $text); });
 
 // Wordpress Optimizations
 remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
@@ -67,20 +70,20 @@ remove_action( 'wp_head', 'wlwmanifest_link' );
 remove_action( 'wp_head', 'rsd_link' );
 
 add_action( 'wp_head', function() {
-	echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
+    echo "<script>(function(html){html.className = html.className.replace(/\bno-js\b/,'js')})(document.documentElement);</script>\n";
 }, 0);
 
 // wp_template()
 //      default setup for wordpress php template files
 function wp_template($file, $debug_in_header = false) {
     // When file is called directly, immediately redirect to a 404 page
-    if ( ! defined( 'ABSPATH' ) ) { 
-        header('Location: /error-404-page-not-found'); 
-        die(); 
+    if ( ! defined( 'ABSPATH' ) ) {
+        header('Location: /error-404-page-not-found');
+        die();
     }
 
     // Debugging
-    if ( WP_DEBUG ) { 
+    if ( WP_DEBUG ) {
         $filestamp = function() use ($file) {
                 date_default_timezone_set( 'America/Louisville' );
                 echo "\n<!-- [" . date( 'H:i:s T' ) . '] [' . timer_stop() . ' seconds] [' .
