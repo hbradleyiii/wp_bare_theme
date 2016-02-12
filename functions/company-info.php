41e7b050196eb45ac063648cdf2bc12dd4c345f7
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-02-12 15:29:22 -0500

    Fixes a schema problem.

--- functions/company-info.php
+++ functions/company-info.php
@@ -14,7 +14,7 @@ function get_address() {
     if ( $address != '' ) {
         $address = '
             <address itemscope itemtype="http://schema.org/PostalAddress">
-                <a target="_blank" href="https://www.google.com/maps/place/' . str_replace(" ", "+", $address.$city.$state.$zipcode) . '" itemprop="address">
+                <a target="_blank" href="https://www.google.com/maps/place/' . str_replace(" ", "+", $address.$city.$state.$zipcode) . '">
                     <span itemprop="streetAddress"> ' . $address . ' </span>
                     <span itemprop="addressLocality"> ' . $city . ', </span>
                     <span itemprop="addressRegion"> ' . $state . ' </span>
