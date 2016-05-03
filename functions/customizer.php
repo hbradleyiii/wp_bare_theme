26e66ffa451f981a9d1fdb37981ca195eaaa8c03
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-05-03 11:19:58 -0400

    Makes function for creating customizer controls.

--- functions/customizer.php
+++ functions/customizer.php
@@ -3,6 +3,28 @@
 ////////////////////////////////////////////////////////////
 // functions/customizer.php - Customizer hooks and functions
 
+
+// wp_add_controls()
+//      Takes an array of contorl=>labels and a section and sets up the
+//      controls for the customizer.
+function wp_add_controls($wp_customize, $section, $controls, $type) {
+    foreach ($controls as $control => $label) {
+        $wp_customize->add_setting($control);
+        $wp_customize->add_control(
+            new WP_Customize_Control(
+                $wp_customize,
+                $control . '_control',
+                array(
+                    'label'       => $label,
+                    'section'     => $section,
+                    'settings'    => $control,
+                    'type'        => $type,
+                )
+            )
+        );
+    }
+}
+
 add_action( 'customize_register', function($wp_customize) {
     $wp_customize->add_section( 'social_media', array(
         'title' => 'Social Media Links',
@@ -13,21 +35,7 @@ add_action( 'customize_register', function($wp_customize) {
         'link_twitter'       => 'Twitter URL',
         'link_youtube'       => 'Youtube URL',
     );
-    foreach ($controls as $control => $label) {
-        $wp_customize->add_setting($control);
-        $wp_customize->add_control(
-            new WP_Customize_Control(
-                $wp_customize,
-                $control . '_control',
-                array(
-                    'label'       => $label,
-                    'section'     => 'social_media',
-                    'settings'    => $control,
-                    'type'        => 'url'
-                )
-            )
-        );
-    }
+    wp_add_controls( $wp_customize, 'social_media', $controls, 'url' );
 });
 
 ?>
