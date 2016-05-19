d274d395c6514e5dacbc7f4d69914a3b3c5151a4
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-05-19 11:28:26 -0400

    Removes pagination from archive/search templates.
    Will put in content.

--- archive.php
+++ archive.php
@@ -4,19 +4,10 @@
 // archive.php - default archive template
 
 get_header(); ?>
-    <main>
-        <header class="page_header">
-            <?php the_archive_title( '<h1 class="page_title">', '</h1>' ); ?>
-        </header>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'content', get_post_format() ); ?>
-        <?php endwhile;
-        the_posts_pagination( array(
-            'prev_text'          => 'Previous page',
-            'next_text'          => 'Next page',
-            'before_page_number' => '<span class="meta-nav screen-reader-text">' . 'Page' . ' </span>',
-        ) );
-        else : // No content found
+    <main style="background-image: url( '<?php echo get_template_directory_uri(); ?>/images/office.jpg' )"><?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+            get_template_part( 'content', get_post_format() );
+        endwhile; else : // No content found
             get_template_part( 'content', 'none' );
         endif; ?>
     </main><?php
--- search.php
+++ search.php
@@ -4,15 +4,10 @@
 // search.php - default search display
 
 get_header(); ?>
-    <main>
-        <?php if ( have_posts() ) :
-            get_template_part( 'content', 'search' );
-            the_posts_pagination( array(
-                'prev_text'          => 'Previous page',
-                'next_text'          => 'Next page',
-                'before_page_number' => '<span class="meta-nav screen-reader-text">' . 'Page' . ' </span>',
-            ) );
-        else : // No content found
+    <main style="background-image: url( '<?php echo get_template_directory_uri(); ?>/images/office.jpg' )"><?php
+        if ( have_posts() ) : while ( have_posts() ) : the_post();
+            get_template_part( 'content', get_post_format() );
+        endwhile; else : // No content found
             get_template_part( 'content', 'none' );
         endif; ?>
     </main><?php
