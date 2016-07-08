09c62d9cf18efd16b670e485331047e84bef472c
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-07-08 10:07:18 -0400

    Adds basic compass configuration file.

---
+++ config.rb
@@ -0,0 +1,10 @@
+http_path = "/"
+css_dir = "."
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+output_style = :nested
+relative_assets = true
+sass_options = { :debug_info => true }
+sourcemap = true
