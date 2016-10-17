f5c17472637cdb63944b7b6de0b172bd27bdce84
Harold Bradley III <hbradleyiii@bradleystudio.net>
2016-10-17 14:15:15 -0400

    Removes uncommon code.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  */
 
 get_header(); ?>
-    <main style="background-image: url( '<?php echo get_template_directory_uri(); ?>/images/office.jpg' )"><?php
+    <main><?php
         if ( have_posts() ) : while ( have_posts() ) : the_post();
             get_template_part( 'content', get_post_format() );
         endwhile; else : // No content found
